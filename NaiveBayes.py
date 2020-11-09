d4c36450d4db428b1578be3279af56950bf3e017
sshipra319 <56846583+sshipra319@users.noreply.github.com>
2020-11-08 23:20:33 -0600

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -28,8 +28,7 @@ def preprocess_text(text):
                   "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", 
                   "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", 
                   "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", 
-                  "now"]
-    
+                  "now"]    
     for j in stop_words:
         text = text.replace(j, '')
     return text
